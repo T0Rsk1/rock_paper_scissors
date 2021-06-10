0810c576e26d33298e5fb87ca329f87608704fb3
Tristan Orlowski <tristan_orlowski@hotmail.com>
2021-06-10 14:38:41 -0400

    Added no-hover class Cleaned up and removed unnecessary code Replaced console.log statements with displayMsg

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const game = document.querySelectorAll('.button');
+const message = document.querySelector('#message');
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
 let playerScore = 0;
 let compScore = 0;
 let end = false;
 
 function computerPlay(){
     const randNum = Math.floor(Math.random()*3);
-    if(randNum === 0) return 'rock';
-    if(randNum === 1) return 'paper';
-    return 'scissors';
+    if(randNum === 0) return 'Rock';
+    if(randNum === 1) return 'Paper';
+    return 'Scissors';
 }
 
 function capitalize(str){
@@ -16,75 +19,65 @@ function capitalize(str){
 
 function playRound(playerSelection, computerSelection){
     playerSelection = capitalize(playerSelection);
-    computerSelection = capitalize(computerSelection);
     
     if(playerSelection === computerSelection){
-        console.log('You tied. ' + playerSelection + ' and ' + computerSelection + ' are the same.');
-        return;
+        displayMsg('No score. You tied. ' + playerSelection + ' and ' + computerSelection + ' are the same.');
+        return null;
     }
 
     if(playerSelection === 'Rock' && computerSelection === 'Scissors'||
        playerSelection === 'Paper' && computerSelection === 'Rock'||
        playerSelection === 'Scissors' && computerSelection === 'Paper'){
-           displayMsg('You win!!!! ' + playerSelection + ' beats ' + computerSelection + '!');
-           return 1;
+           displayMsg('You scored! ' + playerSelection + ' beats ' + computerSelection + '!');
+           return 'p';
     }
 
-    console.log('You lose! ' + computerSelection + ' beats ' + playerSelection + '!');
-    return 0;
+    displayMsg('Computer scored! ' + computerSelection + ' beats ' + playerSelection + '!');
+    return 'c';
 }
 
 function updateScore(e){
     let winner = playRound(e.target.id, computerPlay());
-    
-    if(winner === 1){;
-        playerScore++;
-        return 'PLAYER SCORESSSS!!!!!!!';
-    }
-    else if(winner === 0){
-        compScore++;
-        return 'COMPUTER SCORESSSS!';
-    }
-    else return 'NO SCORE!';
+    if(winner === 'p') playerScore++;
+    else if(winner === 'c') compScore++;
 }
 
 function displayScore(){
-    const player = document.querySelector('#player');
-    const computer = document.querySelector('#computer');
     player.textContent = `PLAYER: ${playerScore}`;
     computer.textContent = `COMPUTER: ${compScore}`;
 }
 
 function displayMsg(msg){
-    const message = document.querySelector('#message');
     message.textContent = `${msg}`;
 }
 
 function declareWinner(){
     if(playerScore === 5 || compScore === 5){
-        if(playerScore > compScore) console.log('PLAYER ONE WINS!!!!!!!!!!!');
-        else console.log('Computer wins.');
+        if(playerScore > compScore) displayMsg('PLAYER WINS!');
+        else displayMsg('Computer wins.');
         return true;
     }
+    return false;
 }
 
 function endGame(){
-    game.forEach(btn => btn.removeEventListener('click', playGame));
-    console.log('GAME OVER');
-    console.log('Press ENTER to reset');
+    game.forEach(btn =>{
+        btn.classList.add('no-hover');
+        btn.removeEventListener('click', playGame);
+    });
+    setTimeout(() => displayMsg('GAME OVER \nPress ENTER to reset'), 1000);
     window.addEventListener('keydown', e => {
         if(e.keyCode === 13) window.location.reload();
     });
 }
 
 function playGame(e){
-    let msg = updateScore(e);
-    displayMsg(msg);
+    updateScore(e);
     displayScore();
     end = declareWinner();
-    console.log(end);
     if(end) endGame();
 }
 
+displayMsg('PLAY GAME!!!');
 displayScore();
 game.forEach(btn => btn.addEventListener('click', playGame));
